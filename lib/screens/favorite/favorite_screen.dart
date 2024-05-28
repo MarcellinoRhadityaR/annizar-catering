0afe9a0a3fbca1d820f97d96b738a4767cdaa410
Nadea Mefira <148459821+nadeamfr@users.noreply.github.com>
2024-05-28 12:40:34 +0800

    Update favorite_screen.dart
    Update favorite_screen.dart

--- lib/screens/favorite/favorite_screen.dart
+++ lib/screens/favorite/favorite_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:catering6/components/product_card.dart';
-import 'package:catering6/models/Product.dart';
-
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../home/components/favorites_provider.dart';
 import '../details/details_screen.dart';
 
 class FavoriteScreen extends StatelessWidget {
@@ -19,28 +19,37 @@ class FavoriteScreen extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16),
-              child: GridView.builder(
-                itemCount: demoProducts.length,
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 200,
-                  childAspectRatio: 0.7,
-                  mainAxisSpacing: 20,
-                  crossAxisSpacing: 16,
-                ),
-                itemBuilder: (context, index) => ProductCard(
-                  product: demoProducts[index],
-                  onPress: () => Navigator.pushNamed(
-                    context,
-                    DetailsScreen.routeName,
-                    arguments:
-                        ProductDetailsArguments(product: demoProducts[index]),
-                  ),
-                ),
+              child: Consumer<FavoritesProvider>(
+                builder: (context, favoritesProvider, child) {
+                  return GridView.builder(
+                    itemCount: favoritesProvider.favoriteProducts.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      childAspectRatio: 0.7,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 16,
+                    ),
+                    itemBuilder: (context, index) {
+                      final product =
+                          favoritesProvider.favoriteProducts[index];
+                      return ProductCard(
+                        product: product,
+                        onPress: () => Navigator.pushNamed(
+                          context,
+                          DetailsScreen.routeName,
+                          arguments: ProductDetailsArguments(product: product),
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
             ),
-          )
+          ),
         ],
       ),
     );
   }
 }
+
