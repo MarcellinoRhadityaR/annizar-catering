4dd6f6b3f86a6e26a9af2f31bb588713532c68ea
Nadea Mefira <148459821+nadeamfr@users.noreply.github.com>
2024-05-28 12:49:42 +0800

    Update package_description.dart
    Update package_description.dart

--- lib/screens/details/components/package_description.dart
+++ lib/screens/details/components/package_description.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-//import '../../../constants.dart';
 import '../../../models/Package.dart';
 
 class PackageDescription extends StatelessWidget {
@@ -26,31 +23,6 @@ class PackageDescription extends StatelessWidget {
             style: Theme.of(context).textTheme.titleLarge,
           ),
         ),
-        Align(
-          alignment: Alignment.centerRight,
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            width: 48,
-            decoration: BoxDecoration(
-              color: package.isFavourite
-                  ? const Color(0xFFFFE6E6)
-                  : const Color(0xFFF5F6F9),
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20),
-                bottomLeft: Radius.circular(20),
-              ),
-            ),
-            child: SvgPicture.asset(
-              "assets/icons/Heart Icon_2.svg",
-              colorFilter: ColorFilter.mode(
-                  package.isFavourite
-                      ? const Color(0xFFFF4848)
-                      : const Color(0xFFDBDEE4),
-                  BlendMode.srcIn),
-              height: 16,
-            ),
-          ),
-        ),
         Padding(
           padding: const EdgeInsets.only(
             left: 20,
@@ -61,30 +33,6 @@ class PackageDescription extends StatelessWidget {
             maxLines: 3,
           ),
         ),
-        // Padding(
-        //   padding: const EdgeInsets.symmetric(
-        //     horizontal: 20,
-        //     vertical: 12,
-        //   ),
-        //   child: GestureDetector(
-        //     onTap: () {},
-        //     child: const Row(
-        //       children: [
-        //         Text(
-        //           "See More Detail",
-        //           style: TextStyle(
-        //               fontWeight: FontWeight.w600, color: kPrimaryColor),
-        //         ),
-        //         SizedBox(width: 5),
-        //         Icon(
-        //           Icons.arrow_forward_ios,
-        //           size: 12,
-        //           color: kPrimaryColor,
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // )
       ],
     );
   }
