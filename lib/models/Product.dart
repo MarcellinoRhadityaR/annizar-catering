614d3c4f2e2e5278a3ab4494293e26296e2889f0
Nadea Mefira <148459821+nadeamfr@users.noreply.github.com>
2024-05-28 12:20:07 +0800

    Update Product.dart

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/material.dart';
-
 class Product {
   final int id;
   final String title, description;
   final List<String> images;
   final int price;
-  final bool isFavourite, isPopular;
-
+  bool isFavourite;
+  final bool isPopular;
+  int quantity = 1;
+  
   Product({
     required this.id,
     required this.images,
@@ -16,10 +16,12 @@ class Product {
     required this.price,
     required this.description,
   });
+  void toggleFavoriteStatus() {
+    isFavourite = !isFavourite;
+  }
 }
 
 // Our demo Products
-
 List<Product> demoProducts = [
   Product(
     id: 1,
@@ -28,8 +30,7 @@ List<Product> demoProducts = [
     ],
     title: "Es Krim",
     price: 9000,
-    description: description,
-    isFavourite: true,
+    description: "Es Krim yang terbuat dari campuran susu, gula, dan bahan perasa lainnya dengan tekstur yang lembut dan dingin.",
     isPopular: true,
   ),
   Product(
@@ -39,8 +40,7 @@ List<Product> demoProducts = [
     ],
     title: "Nasi Putih",
     price: 5000,
-    description: description,
-    isPopular: true,
+    description: "Nasi Putih yang dimasak dari beras pilihan hingga empuk dan pulen.",
   ),
   Product(
     id: 3,
@@ -48,10 +48,8 @@ List<Product> demoProducts = [
       "assets/images/esteh.png",
     ],
     title: "Es Teh",
-    price: 3655,
-    description: description,
-    isFavourite: true,
-    isPopular: true,
+    price: 3000,
+    description: "Es Teh yang terbuat dari teh alami yang diseduh dan didinginkan dengan es.",
   ),
   Product(
     id: 4,
@@ -59,9 +57,8 @@ List<Product> demoProducts = [
       "assets/images/sambal.png",
     ],
     title: "Sambal Pedas",
-    price: 2020,
-    description: description,
-    isFavourite: true,
+    price: 2000,
+    description: "Sambal Pedas yang terbuat dari cabai, bawang, dan rempah-rempah lainnya sehingga semakin terasa lezatnya.",
   ),
   Product(
     id: 1,
@@ -69,9 +66,8 @@ List<Product> demoProducts = [
       "assets/images/ayam.png",
     ],
     title: "Ayam Goreng",
-    price: 6499,
-    description: description,
-    isFavourite: true,
+    price: 7000,
+    description: "Ayam Goreng yang digoreng dengan dibaluri tepung bumbu hingga kulitnya renyah dan dagingnya lembut.",
     isPopular: true,
   ),
   Product(
@@ -80,8 +76,8 @@ List<Product> demoProducts = [
       "assets/images/cumi.png",
     ],
     title: "Cumi Pedas",
-    price: 505,
-    description: description,
+    price: 9000,
+    description: "Cumi Pedas yang dimasak dengan bumbu pedas memiliki cita rasa yang kuat dan sering disajikan dengan nasi putih.",
     isPopular: true,
   ),
   Product(
@@ -90,10 +86,8 @@ List<Product> demoProducts = [
       "assets/images/tempe.png",
     ],
     title: "Tempe Goreng",
-    price: 3655,
-    description: description,
-    isFavourite: true,
-    isPopular: true,
+    price: 2000,
+    description: "Tempe Goreng yang dimarinasi menggunakan rempah-rempah pilihan dan digoreng hingga renyah.",
   ),
   Product(
     id: 4,
@@ -101,9 +95,8 @@ List<Product> demoProducts = [
       "assets/images/tahu.png",
     ],
     title: "Tahu Goreng",
-    price: 2020,
-    description: description,
-    isFavourite: true,
+    price: 2000,
+    description: "Tahu Goreng yang dimarinasi menggunakan rempah-rempah pilihan dan digoreng hingga berwarna keemasan dan renyah di luar, namun lembut di dalam. .",
   ),
   Product(
     id: 1,
@@ -111,9 +104,9 @@ List<Product> demoProducts = [
       "assets/images/gurami.png",
     ],
     title: "Gurami Pedas",
-    price: 2020,
-    description: description,
-    isFavourite: true,
+    price: 12000,
+    description: "Gurami Pedas yang dimasak dengan bumbu pedas hingga meresap sempurna.",
+    isPopular: true,
   ),
   Product(
     id: 2,
@@ -121,9 +114,8 @@ List<Product> demoProducts = [
       "assets/images/nasigoreng.png",
     ],
     title: "Nasi Goreng",
-    price: 2020,
-    description: description,
-    isFavourite: true,
+    price: 10000,
+    description: "Nasi Goreng yang digoreng dengan campuran bumbu, sayuran, dan dapat ditambah dengan telur, daging, atau seafood.",
   ),
   Product(
     id: 3,
@@ -131,9 +123,9 @@ List<Product> demoProducts = [
       "assets/images/udang.png",
     ],
     title: "Udang Pedas",
-    price: 2020,
-    description: description,
-    isFavourite: true,
+    price: 10000,
+    description: "Udang Pedas yang dimasak dengan saus pedas. Hidangan ini menggabungkan rasa manis dan pedas.",
+    isPopular: true,
   ),
   Product(
     id: 4,
@@ -141,11 +133,9 @@ List<Product> demoProducts = [
       "assets/images/perkedel.png",
     ],
     title: "Perkedel Goreng",
-    price: 2020,
-    description: description,
-    isFavourite: true,
+    price: 4000,
+    description: "Bola-bola Kentang yang dicampur dengan daging cincang atau sayuran, kemudian digoreng hingga renyah.",
+    isPopular: true,
   ),
 ];
 
-const String description =
-    "Wireless Controller for PS4™ gives you what you want in your gaming from over precision control your games to sharing …";
