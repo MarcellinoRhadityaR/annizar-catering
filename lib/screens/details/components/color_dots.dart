2c4854c11697323767f1ac0ca5461310df194813
Nadea Mefira <148459821+nadeamfr@users.noreply.github.com>
2024-06-07 20:14:25 +0700

    Update color_dots.dart
    Update color_dots.dart

--- lib/screens/details/components/color_dots.dart
+++ lib/screens/details/components/color_dots.dart
@@ -34,37 +34,3 @@ class ColorDots extends StatelessWidget {
   }
 }
 
-class ColorDot extends StatelessWidget {
-  const ColorDot({
-    super.key,
-    required this.color,
-    this.isSelected = false,
-    required this.product,
-  });
-
-  final Color color;
-  final bool isSelected;
-  final Product product;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 2),
-      padding: const EdgeInsets.all(8),
-      height: 40,
-      width: 40,
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        border:
-            Border.all(color: isSelected ? kPrimaryColor : Colors.transparent),
-        shape: BoxShape.circle,
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: color,
-          shape: BoxShape.circle,
-        ),
-      ),
-    );
-  }
-}
